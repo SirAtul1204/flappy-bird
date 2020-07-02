3ff56c0421c4c83653218ad24710943bd2fb1258
siratul <asatulsingh20@gmail.com>
2020-07-02 12:32:42 +0530

    Updates

---
+++ precache-manifest.0b3c60c3cb3ad07c6c7e9106aedeb046.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54dfd7fe3fa8f7cfb4d7e64c160180fb",
+    "url": "/flappy-bird/index.html"
+  },
+  {
+    "revision": "b0c56ad476c42efdbbee",
+    "url": "/flappy-bird/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "03c82cdb84d863b6f13f",
+    "url": "/flappy-bird/static/css/main.1dbc5342.chunk.css"
+  },
+  {
+    "revision": "b0c56ad476c42efdbbee",
+    "url": "/flappy-bird/static/js/2.d0bd0b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flappy-bird/static/js/2.d0bd0b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c82cdb84d863b6f13f",
+    "url": "/flappy-bird/static/js/main.b2f1c5d1.chunk.js"
+  },
+  {
+    "revision": "82a1a6e8669dd1e4b881",
+    "url": "/flappy-bird/static/js/runtime-main.52d7bf52.js"
+  },
+  {
+    "revision": "c77f418ed25c73b6a272aea98e3bfed2",
+    "url": "/flappy-bird/static/media/bird.c77f418e.png"
+  }
+]);
